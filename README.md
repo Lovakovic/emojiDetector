0817374dc9f20370d989cdc16fd3d5b0e0c2ba84
Lovakovic <lovakovdario@gmail.com>
2023-01-08 17:22:41 +0100

    Saving an almost working model before messing with Chat-GPT.

---
+++ README.md
@@ -0,0 +1,4 @@
+To run tensorflow-gpu you also need CUDA 11.2 and cuDNN version 8.1.
+
+If you're running the program on windows, do not upgrade tensorflow library to a version newer than 2.10.0, as newer
+versions dropped support for GPU acceleration on Windows.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import os
 from tensorflow import keras
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten
 
 
 def limit_vram_usage():
@@ -36,5 +37,48 @@ if __name__ == '__main__':
     # Load the label for each image
     img_labels = load_labels()
 
+    # Load the data (shuffles it by default)
     train_dir = os.path.join('data', 'train')
     data = keras.utils.image_dataset_from_directory(train_dir)
+
+    # iterator = train_data.as_numpy_iterator()
+    # batch = iterator.next()
+    # print(batch[0].shape)
+
+    # Scale the color values down to improve neural network efficiency
+    data = data.map(lambda img, label: (img / 255, label))
+
+    train_size = int(len(data) * .7) + 1
+    val_size = int(len(data) * .2)
+    test_size = int(len(data) * .1)
+    # print(str(len(data)) + ' ' + str(train_size) + ' ' + str(val_size) + ' ' + str(test_size))
+
+    # Split the data
+    train = data.take(train_size)
+    val = data.skip(train_size).take(val_size)
+    test = data.skip(train_size + val_size).take(test_size)
+
+    model = keras.models.Sequential([
+        # 16 filters, 3x3px filter size, 1px stride between filters
+        Conv2D(16, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)),
+        MaxPool2D(),
+
+        # 32 filters, 3x3px filter size, 1px stride between filters
+        Conv2D(32, (3, 3), 1, activation='relu'),
+        MaxPool2D(),
+
+        # 16 filters, 3x3px filter size, 1px stride between filters
+        Conv2D(16, (3, 3), 1, activation='relu'),
+        MaxPool2D(),
+
+        Flatten(),
+
+        Dense(256, activation='relu'),
+
+        # 207 different emojis
+        Dense(207, activation='sigmoid')
+    ])
+
+    model.compile(optimizer='adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+
+    history = model.fit(train, epochs=20, validation_data=val, verbose=1)
